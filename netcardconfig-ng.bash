b9c3037880bac892bac8fd06778a999028211e7c
Kel Modderman <kel@otaku42.de>
2006-06-11 00:46:55 +0000

    Use lib function files

--- netcardconfig-ng.bash
+++ netcardconfig-ng.bash
@@ -4,6 +4,13 @@ export PATH="/bin:/sbin:/usr/bin:/usr/sbin"
 
 #((UID)) && exec kanotix-su $0 "$@"
 
+#LIBPATH=/usr/lib/netcardconfig-kanotix
+LIBPATH=$PWD
+
+source $LIBPATH/get_netdev_functions || exit 1
+
+source $LIBPATH/display_ssft_functions || exit 1
+
 source ssft.sh || exit 1
 
 if [[ $DISPLAY ]]
@@ -13,75 +20,23 @@ else
 	export SSFT_FRONTEND="dialog"
 fi
 
-function error () {
-	ssft_display_error ERROR "$@"
-	exit 1
-}
-
-function get_valid_netdevs () {
-	local count iface netdev_type
-	for i in /sys/class/net/*
-	do
-		((count++))
-		iface=$(basename $i)
-		# TODO: /usr/lib/netcardconfig-kanotix perhaps
-		netdev_type=$(awk -f $PWD/nicinfo.awk $iface type)
-		case $netdev_type in
-			wired)
-				WIRED_IFACES[$count]=$iface
-				;;
-			wireless)
-				WIRELESS_IFACES[$count]=$iface
-				;;
-		esac
-	done
-}
-
-function get_netdev_desc () {
-	local count iface desc
-	for i in "$@"
-	do
-		((count++))
-		iface=$i
-		desc=$(awk -f $PWD/nicinfo.awk $iface desc)
-		IFACE[$count]="$iface    [$desc]"
-	done
-}
-
-function display_netdev_wire_or_wifi () {
-	local tit msg
-	tit="Network Type"
-	msg="What type of network would you like to setup?"
-	ssft_select_single "$tit" "$msg" "$@"
-}
-
-function display_netdev_list () {
-	local tit msg type
-	type=$1
-	shift
-	tit="Choose Network Device"
-	msg="What $type device is to be configured?"
-	ssft_select_single "$tit" "$msg" "$@"
-}
-
-get_valid_netdevs
+# Populates two arrays, ${WIRED_IFACES[*]} and ${WIRELESS_IFACES[*]}
+get_netdev_valid_ifaces
 
 if [[ ! ${WIRED_IFACES[*]} && ! ${WIRELESS_IFACES[*]} ]]
 then
-	error "No valid network devices detected"
-fi
-
-if [[ ! ${WIRELESS_IFACES[*]} ]]
+	display_error "No valid network devices detected"
+elif [[ ! ${WIRELESS_IFACES[*]} && ${WIRED_IFACES[*]} ]]
 then
 	TYPE=LAN
 	get_netdev_desc ${WIRED_IFACES[*]}
-elif [[ ! ${WIRED_IFACES[*]} ]]
+elif [[ ! ${WIRED_IFACES[*]} && ${WIRELESS_IFACES[*]} ]]
 then
 	TYPE=WiFi
 	get_netdev_desc ${WIRELESS_IFACES[*]}
 else
 	SSFT_DEFAULT="Local Area Network"
-	if display_netdev_wire_or_wifi  "Local Area Network" "Wireless Network"
+	if display_wired_or_wireless  "Local Area Network" "Wireless Network"
 	then
 		case "$SSFT_RESULT" in
 			"Local Area Network")
@@ -96,7 +51,7 @@ else
 	fi
 fi
 
-if display_netdev_list "$TYPE" "${IFACE[@]}"
+if display_iface_list "$TYPE" "${IFACE[@]}"
 then
 	IFACE=${SSFT_RESULT%% *}
 fi
